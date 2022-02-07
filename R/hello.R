2c09331e3b6caeacc757cef08f475d89e2e92820
Don Boyd <donboyd5@gmail.com>
2022-02-07 18:08:32 -0500

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,35 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+
+# library(devtools)
+# use_git()
+# use_r("strsplit1")
+# load_all()
+# check()
+# use_devtools() # opens .Rprofile, which is here: C:/Users/donbo/Documents/.Rprofile
+# devtools::install_github("r-lib/devtools")
+# devtools::install_github("r-lib/usethis")
+# usethis::use_build_ignore("notes")
+
+# if (interactive()) {
+#   suppressMessages(require(devtools))
+# }
+
+
